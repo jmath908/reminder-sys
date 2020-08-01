b61a90c28d57d65b182363c3c0662b514a7dbb67
jmath908 <40603199+jmath908@users.noreply.github.com>
2020-08-01 16:00:38 -0400

    Update reminders.py

--- reminders.py
+++ reminders.py
@@ -1,17 +1,6 @@
 #A reminder system for timed events
 
-##toaster.show_toast("Hello World!!!",
-##                   "Python is 10 seconds awsm!",
-##                   icon_path="custom.ico",
-##                   duration=10)
-##
-##toaster.show_toast("Example two",
-##                   "This notification is in it's own thread!",
-##                   icon_path=None,
-##                   duration=5,
-##                   threaded=True)
-# Wait for threaded notification to finish
-##while toaster.notification_active(): time.sleep(0.1)
+
 from win10toast import ToastNotifier
 toaster = ToastNotifier()
 import time
@@ -25,7 +14,7 @@ if(val=="y"):
     time.sleep(remT*60)
     toaster.show_toast(remindName,
                    remindDesc,
-                   icon_path="stcross.png",
+                   icon_path="",
                    duration=remT)
     while toaster.notification_active(): time.sleep(0.1)
 
